085b4ffa73acdf45b1b940d573091270758f744d
cedpeters <sashadaisy1999@gmail.com>
2020-09-24 23:30:29 +0100

    cleanup

--- api/app.js
+++ api/app.js
@@ -4,13 +4,9 @@ const app = express();
 const hostname = '127.0.0.1';
 const port = 3000;
 
-// app.get('/hello', async (req, res) => {
-//     res.status(200).send('Hello World!');
-// });
-
-app.get('/Hello', async (req, res) => {
-  res.send('Hello World!')
-})
+app.get('/hello', async (req, res) => {
+    res.status(200).send('Hello World!');
+});
 
 const server = app.listen(port, hostname, () => console.log(`App listening at http://localhost:${port}`));
 
