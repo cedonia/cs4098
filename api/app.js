e127b8e3d31161d505ab93489c97e37094f2ebde
cedpeters <sashadaisy1999@gmail.com>
2020-09-24 23:27:45 +0100

    make basic endpoint work

--- api/app.js
+++ api/app.js
@@ -1,14 +1,17 @@
-const http = require('http');
 const express = require('express');
 const app = express();
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-app.get('/', async (req, res) => {
-    res.status(200).send('Hello World!');
-});
+// app.get('/hello', async (req, res) => {
+//     res.status(200).send('Hello World!');
+// });
 
-const server = app.listen(port, hostname, () => console.log('App listening at http://localhost:${port}'));
+app.get('/Hello', async (req, res) => {
+  res.send('Hello World!')
+})
+
+const server = app.listen(port, hostname, () => console.log(`App listening at http://localhost:${port}`));
 
 module.exports = server;
