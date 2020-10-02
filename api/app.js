df18874a2c898cb1f596614d05637cf543da6dff
cmp24 <cmp24@lyrane.cs.st-andrews.ac.uk>
2020-10-02 10:29:46 +0100

    update port

--- api/app.js
+++ api/app.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 21257;
 
 app.get('/hello', async (req, res) => {
     res.status(200).send('Hello World!');
 });
 
-const server = app.listen(port, hostname, () => console.log(`App listening at http://localhost:${port}`));
+const server = app.listen(port, hostname, () => console.log(`App listening at http://${hostname}:${port}`));
 
 module.exports = server;
