81604588e72aa8726ec19b1cbc3258f333d405a2
cmp24 <cmp24@lyrane.cs.st-andrews.ac.uk>
2020-10-02 11:01:14 +0100

    small update

--- api/app.js
+++ api/app.js
@@ -4,8 +4,12 @@ const app = express();
 const hostname = '127.0.0.1';
 const port = 21257;
 
-app.get('/', async(req, res) => {
+app.get('/nano', async(req, res) => {
 	res.status(200).send('Welcome to this app');
+});
+
+app.get('/', async(req, res) => {
+	res.status(200).send("Welcome to the base page of the app");
 });
 
 app.get('/hello', async (req, res) => {
