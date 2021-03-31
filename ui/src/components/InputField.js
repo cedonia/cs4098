031058e1c07ad90128ebc593156f87c132d033d9
cedpeters <sashadaisy1999@gmail.com>
2021-03-31 19:16:40 +0100

    remove unneeded dependencies

--- ui/src/components/InputField.js
+++ ui/src/components/InputField.js
@@ -3,14 +3,9 @@ import {TextField, Button, Select, MenuItem} from '@material-ui/core';
 import axios from 'axios';
 import Confirmation from './Confirmation.js';
 import { withStyles } from '@material-ui/core/styles';
-import { green } from '@material-ui/core/colors';
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
-import CheckBoxIcon from '@material-ui/icons/CheckBox';
-import Favorite from '@material-ui/icons/Favorite';
-import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
 
 class InputField extends React.Component {
 
