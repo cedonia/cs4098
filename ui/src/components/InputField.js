bc75d47897884dcc7d71cc8c9c55096aa6263bd2
cedpeters <sashadaisy1999@gmail.com>
2021-03-31 17:37:07 +0100

    Add checkboxes to UI

--- ui/src/components/InputField.js
+++ ui/src/components/InputField.js
@@ -2,6 +2,15 @@ import React from 'react';
 import {TextField, Button, Select, MenuItem} from '@material-ui/core';
 import axios from 'axios';
 import Confirmation from './Confirmation.js';
+import { withStyles } from '@material-ui/core/styles';
+import { green } from '@material-ui/core/colors';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
+import CheckBoxIcon from '@material-ui/icons/CheckBox';
+import Favorite from '@material-ui/icons/Favorite';
+import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
 
 class InputField extends React.Component {
 
@@ -11,7 +20,14 @@ class InputField extends React.Component {
     this.state = {
       value: this.props.initialText,
       redirect: false,
-      podcastId: ''
+      podcastId: '',
+      mon: false,
+      tues: false,
+      wed: false,
+      thurs: false,
+      fri: false,
+      sat: false,
+      sun: false
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -32,10 +48,33 @@ class InputField extends React.Component {
     this.setState({title: encodeURIComponent(event.target.value)});
   }
 
-//https://cmp24.host.cs.st-andrews.ac.uk/api/GenPodcast/title/autumn?mon=false&tues=false&wed=false&thurs=false&fri=false&sat=false&sun=true
-  handleSubmit(event) {
+  handleMonChange(event) {
+    this.setState({mon: event.target.checked});
+  }
+  handleTuesChange(event) {
+    this.setState({tues: event.target.checked});
+  }
+  handleWedChange(event) {
+    this.setState({wed: event.target.checked});
+  }
+  handleThursChange(event) {
+    this.setState({thurs: event.target.checked});
+  }
+  handleFriChange(event) {
+    this.setState({fri: event.target.checked});
+  }
+  handleSatChange(event) {
+    this.setState({sat: event.target.checked});
+  }
+  handleSunChange(event) {
+    this.setState({sun: event.target.checked});
+  }
 
-    axios.get(this.apiLink + '/api/GenPodcast/title/' + encodeURIComponent(this.state.title) + '?mon=true&tues=false&wed=false&thurs=false&fri=false&sat=false&sun=false',
+
+//https://cmp24.host.cs.st-andrews.ac.uk/api/GenPodcast/title/autumn?mon=false&tues=false&wed=false&thurs=false&fri=false&sat=false&sun=true
+  handleSubmit() {
+
+    axios.get(this.apiLink + '/api/GenPodcast/title/' + encodeURIComponent(this.state.title) + '?mon=' + this.state.mon + '&tues=' + this.state.tues + '&wed=' + this.state.wed + '&thurs=' + this.state.thurs + '&fri=' + this.state.fri + '&sat=' + this.state.sat + '&sun=' this.state.sun,
     {
       method: 'GET',
       headers: {
@@ -54,6 +93,16 @@ class InputField extends React.Component {
       });
     });
   }
+
+  const greenCheckbox = withStyles({
+    root: {
+      color: green[400],
+      '&$checked': {
+        color: green[600],
+      },
+    },
+    checked: {},
+  })((props) => <Checkbox color="default" {...props} />);
 
   render() {
 
@@ -81,22 +130,42 @@ class InputField extends React.Component {
       <TextField id="standard-basic" onChange={this.handleTitleChange} label="Librivox Title" required = {true}/>
       <br/>
       <br/>
-      <p> How often do you want a new chapter?</p>
-      <Select
-          labelId="demo-simple-select-autowidth-label"
-          id="demo-simple-select-autowidth"
-          value={age}
-          onChange={this.handleChange}
-          autoWidth
-        >
-          <MenuItem value={10}>Daily</MenuItem>
-          <MenuItem value={20}>Every Other Day</MenuItem>
-          <MenuItem value={30}>Weekly</MenuItem>
-        </Select>
+      <p> Select the days you want to receive a new chapter.</p>
+      <FormGroup row>
+        <FormControlLabel
+          control={<Checkbox checked={state.mon} onChange={handleMonChange} name="Monday" color="primary"/>}
+          label = "Monday"
+        />
+        <FormControlLabel
+          control={<Checkbox checked={state.tues} onChange={handleTuesChange} name="Tuesday" color="primary" />}
+          label = "Tuesday"
+        />
+        <FormControlLabel
+          control={<Checkbox checked={state.wed} onChange={handleWedChange} name="Wednesday" color="primary" />}
+          label = "Wednesday"
+        />
+        <FormControlLabel
+          control={<Checkbox checked={state.thurs} onChange={handleThursChange} name="Thursday" color="primary" />}
+          label = "Thursday"
+        />
+        <FormControlLabel
+          control={<Checkbox checked={state.fri} onChange={handleFriChange} name="Friday" color="primary" />}
+          label = "Friday"
+        />
+        <FormControlLabel
+          control={<Checkbox checked={state.sat} onChange={handleSatChange} name="Saturday" color="primary" />}
+          label = "Saturday"
+        />
+        <FormControlLabel
+          control={<Checkbox checked={state.sun} onChange={handleSunChange} name="Sunday" color="primary" />}
+          label = "Sunday"
+        />
+      </FormGroup>  
+
       <br/>
       <br/>
       <br/>
-      <Button variant="outlined" onClick = {() => this.handleSubmit("hello")} color="primary">
+      <Button variant="outlined" onClick = {() => this.handleSubmit()} color="primary">
   Submit
 </Button>
       </div>
