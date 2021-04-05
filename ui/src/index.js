61cd58044f1a02fa956e937f29c8e0937e57a35b
cedpeters <sashadaisy1999@gmail.com>
2021-04-05 14:08:22 +0100

    delete App

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
+import Home from './components/Home';
 import * as serviceWorker from './serviceWorker';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
   document.getElementById('root')
 );
