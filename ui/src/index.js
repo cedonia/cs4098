d31eb2a518893c489a0d793e33cb8351e41e89e1
cedpeters <sashadaisy1999@gmail.com>
2021-04-05 14:54:22 +0100

    more file cleanup

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <InputField initialText="Librivox Title" />
   </React.StrictMode>,
   document.getElementById('root')
 );
